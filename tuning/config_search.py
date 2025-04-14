1ee7834e0c06b06c94f00ebb155a2cc531788781
Marco Galão <marcogalao@outlook.pt>
2025-04-14 15:48:30 +0100

    structure repo

---
+++ tuning/config_search.py
@@ -0,0 +1,24 @@
+from evolution.entities import Population
+# from evolution.selection import ...
+# from evolution.crossover import ...
+# from evolution.mutation import ...
+
+import pandas as pd
+
+# --------------------- Hyperparameters to tune -------------------- #
+selection = []
+crossover = []
+mutation = []
+elitism = [True, False]
+
+# ---------------------- Fixed Hyperparameters ---------------------- #
+
+
+
+
+
+def grid_search(selection, crossover, mutation, elitism):
+    ...
+
+
+results.to_csv('config_results.csv')
---
+++ tuning/selection_search.py
@@ -0,0 +1,18 @@
+from evolution.entities import Population
+# from evolution.selection import ...
+# from evolution.crossover import ...
+# from evolution.mutation import ...
+
+import pandas as pd
+
+# --------------- Hyperparameters to choose randomly ---------------- #
+crossover = []
+mutation = []
+
+# ---------------------- Fixed Hyperparameters ---------------------- #
+
+
+
+
+
+results.to_csv('selection_results.csv')

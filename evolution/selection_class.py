a5a67f4d5584db9cbd3aa85ba292864fbf2987aa
margaridabcardoso <margaridabravocardoso@gmail.com>
2025-04-19 20:55:51 +0100

    implementation of selection methods

--- evolution/selection_class.py
+++ evolution/selection_class.py
@@ -1,26 +1,76 @@
 import random
 from copy import deepcopy
-from evolution.solution import Solution
-from evolution.solution import MLFSolution
+from evolution.entities import Solution
+from evolution.entities import Individual
+import os
+import sys
+import random
 
-def fitness_proportionate_selection(population: list[Solution], maximization: bool):
-    total_fitness = sum([ind.fitness() for ind in population])
+# Add the parent directory to the system path to import the import_data module
+parent_folder = os.path.abspath(os.path.join(os.getcwd(), ".."))
+sys.path.append(parent_folder)
 
-    if maximization:
-        fitness_values = [ind.fitness() for ind in population]
-    else:
-        # Minimization: Use the inverse of the fitness value
-        # Lower fitness should have higher probability of being selected
-        fitness_values = [1 / ind.fitness() for ind in population]
+from data import import_data
 
-    total_fitness = sum(fitness_values)
-    # Generate random number between 0 and total
-    random_nr = random.uniform(0, total_fitness)
-    box_boundary = 0
-    # For each individual, check if random number is inside the individual's "box"
-    for ind_idx, ind in enumerate(population):
-        box_boundary += fitness_values[ind_idx]
-        if random_nr <= box_boundary:
-            return deepcopy(ind)
+# Import data from the import_data module
+artists = import_data.artists
+conflicts_matrix = import_data.conflicts_matrix
 
-def 
+def fitness_proportionate_selection(population):
+
+    fitnesses = []
+    probabilities = []
+
+    for indiv in population:  fitnesses.append(indiv.fitness())
+    total_fitness = sum(fitnesses)
+
+    for indiv in population: probabilities.append(indiv.fitness()/total_fitness)
+
+    selected_indiv = random.choices(population, weights=probabilities, k=1)[0]
+
+    return deepcopy(selected_indiv)
+
+def ranking_selection(population):
+    probabilities = []
+    sorted_population = sorted(population, key=lambda indiv: indiv.fitness(), reverse=True)
+
+    ranking_position = range(1,len(population)+1)
+
+    for position in ranking_position: probabilities.append(1-(position/sum(ranking_position)))
+
+    selected_indiv = random.choices(sorted_population, weights=probabilities, k=1)[0]
+
+    return deepcopy(selected_indiv)
+
+
+def tournament_selection(population, tournament_size):
+    tournament_population = random.sample(population, tournament_size)
+    best_indiv = max(tournament_population, key=lambda indiv: indiv.fitness(), reverse=True)
+    return deepcopy(best_indiv)
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
